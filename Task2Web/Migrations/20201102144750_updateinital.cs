3b0e5cb955455d5ebe07c042d40bdef798666d38
Andrey Aktachun <andrey.aktachun@bbit.tech>
2020-11-02 06:50:43 -0800

    aa

---
+++ Task2Web/Migrations/20201102144750_updateinital.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Task2Web.Migrations
+{
+    public partial class updateinital : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LoginModels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LoginModels", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "LoginModels",
+                columns: new[] { "Id", "Password", "UserName" },
+                values: new object[] { 1, "1234", "andrey" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LoginModels");
+        }
+    }
+}
